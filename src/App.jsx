011d3e99e161ecfe301c4c621ee892aebdf4d76a
Web Wise Dynamics <161061255+w2d-ar@users.noreply.github.com>
2024-03-05 09:14:53 +0000

    feat:add tailwindcss theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Test webcode</h1>
+      <h1 className='text-3xl font-bold underline'>Helo</h1>
     </>
   )
 }
