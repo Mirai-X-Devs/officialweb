fe4c31d8e479ab213bdc012ba5d2e032ee5b5d76
Web Wise Dynamics <161061255+w2d-ar@users.noreply.github.com>
2024-03-07 03:20:46 +0000

    fix: update app file with new components
    Co-authored-by: thecoderbutcher <thecoderbutcher@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import Button from './components/Button'; 
-import ButtonGradient from './assets/svg/ButtonGradient';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button
-          className="mt-10"
-          href="#login"
-        >
-          Something
-        </Button>
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
-      <ButtonGradient/>
-    </>
-  )
-}
 
-export default App
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
