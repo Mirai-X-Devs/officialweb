e2832944bccfff9752a21bde457f9d7217ac9308
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-01-25 05:03:21 -0300

    testbuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://thecoderbutcher.github.io/miraidev-officialweb",
   plugins: [react()],
 })
